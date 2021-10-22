e57505e2a91f486aa9ae868bf1d56f6adb13e0a9
urrutialex <87054543+urrutialex@users.noreply.github.com>
2021-10-22 14:28:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function compute() {
-  var principal = document.getElementById("principal").value;
-  var rate = document.getElementById("rate").value;
-  var years = document.getElementById("years").value;
-  var interest = (principal * years * rate) / 100;
-  var year = new Date().getFullYear()+parseInt(years);
-  var result = document.getElementById("result");
+function compute()
+{
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("myRange").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var dueyear = new Date().getFullYear();
+	var myyr = Number(dueyear) + Number(years);
 
-  result.innerHTML = "If you deposit" + principal + "," <br> "at an interest rate of" + rate + "," <br> "You will receive an amount of" + interest + " <br> "in the year" + year + "!";
-
-  if (principal <= 0) {
-    alert("Enter a positive number");
-    principal.focus();
-    return false;
-  }
+    var displayresult = document.getElementById("result");
+    displayresult.innerText =  "If you deposit " + principal + "\n at an interest rate of " + rate + "%. \nYou will receive an interest of " + interest + "\nin the year " + myyr;
 }
 
-function updateRate() {
-  var rate = document.getElementById("rate").value;
-  document.getElementById("rate_span").innerHTML = rate + "%";
+function getmyslider(){
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    
+    output.innerHTML = slider.value + "%";
+	
 }
+       
