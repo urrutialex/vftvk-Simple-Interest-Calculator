c976face4977bf6146b3fa2e65414558bfc0b619
urrutialex <87054543+urrutialex@users.noreply.github.com>
2021-10-22 13:41:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function compute(){
-
-    var principal = document.getElementById("principal").value;
-    if (principal<=0){
-      document.getElementById("result").innerHTML = "Please enter a valid amount.";
-      return;
+function compute()
+{
+    var principal = document.getElementById('principal').value;
+    var rate = document.getElementById('rate').value;
+    var years = document.getElementById('years').value;
+    var result = document.getElementById('result');
+    // Reset result
+    result.innerHTML = '';
+    // Validate values first
+    if (!principal || principal <= 0) {
+        alert('Enter a positive number');
+        // Return focus as indicated in instructions
+        document.getElementById('principal').focus()
+        return;
     }
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = (parseInt(principal) * parseFloat(rate) * parseInt(years))/100;
-    var currentDate = new Date();
-    var year = currentDate.getFullYear();
-    var maturityYear = parseInt(years) + parseInt(year);
-    var maturityValue = parseInt(interest) + parseInt(principal);
-    var output = "If you deposit " + principal +"<br>at an interest rate of " + rate +"%.<br>You will receive an amount of " + maturityValue + "<br> at year " + maturityYear + ".<br>";
-    document.getElementById("result").innerHTML = output;
+    // Calculate interest and year based on values
+    var interest = parseInt(principal) * parseInt(years) * parseFloat(rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    // Don't calculate if either is not a number
+    if (isNaN(interest) || isNaN(year)) {
+        return
+    }
+    // Update result
+    result.innerHTML = 'If you deposit <div class="highlight">' +  principal + '</div>,<br>' + 
+        ' at an interest rate of <div class="highlight">' + rate + '%</div>. <br>' 
+        + 'You will receive an amount of <div class="highlight">' + interest + '</div>, <br>' 
+        + 'in the year <div class="highlight">' + year + '</div><br>';
 }
-function displayRate(){
-    var rate = document.getElementById("rate").value;
-    rate += "%";
-    document.getElementById("displayRate").innerHTML = rate;
-}
+
+function updateRate()
+{
+    var rateval = document.getElementById('rate').value;
+    document.getElementById('rate_val').innerText = rateval + '%';
+}        
