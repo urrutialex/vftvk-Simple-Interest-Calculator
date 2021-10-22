ead8bd41723151824cd6e456615ecceadb760c8f
urrutialex <87054543+urrutialex@users.noreply.github.com>
2021-10-22 14:49:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("myRange").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100;
-    var year = new Date().getFullYear();
-	
+     	var rate = document.getElementById("myRange").value;
+    	var years = document.getElementById("years").value;
+    	var interest = principal * years * rate/100;
+   	var year = new Date().getFullYear();
 	var byyear = Number(year) + Number(years);
-
-    var result = document.getElementById("result");
-    result.innerText =  "If you deposit " + principal + "\n at an interest rate of " + rate + "%. \nYou will receive an interest of " + interest + "\nin the year " + byyear;
-}
+	var principal = document.getElementById("principal").value;
+	if principal <= 0 {
+		alert("Please enter a positive value");
+	} else {
+	
+	var result = document.getElementById("result");
+		result.innerText =  "If you deposit " + principal + "\n at an interest rate of " + rate + "%. \nYou will receive an interest of " + interest + "\nin the year " + byyear;
+}}
 
 function updateRate(){
     var slider = document.getElementById("myRange");
