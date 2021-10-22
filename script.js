733e3c71ae03f170bc543b96a3acfed54ae985a2
urrutialex <87054543+urrutialex@users.noreply.github.com>
2021-10-22 10:34:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
+
 
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    
+    
+    
+    
+    
+    
     p = document.getElementById("principal").value;
     document.getElementById("result").innerHTML =
     "If you deposit <mark>" + principal + "</mark>,<br />" +
