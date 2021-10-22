7bf522589861728e143d465ecb924de10b19acfa
urrutialex <87054543+urrutialex@users.noreply.github.com>
2021-10-22 13:02:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function compute()
 {
     principal = document.getElementById("principal").value;
-    if (principal<=0) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-    }
-    else{
-        rate = document.getElementById("rate").value;
-        years = document.getElementById("years").value;
-        interest = principal * years * rate /100;
-        year = new Date().getFullYear()+parseInt(years);
+    
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    interest = principal * years * rate /100;
+    year = new Date().getFullYear()+parseInt(years);
         
-        document.getElementById("result").innerHTML = "If you deposit ";
+    document.getElementById("result").innerHTML = "If you deposit ";
     }
 }
 
